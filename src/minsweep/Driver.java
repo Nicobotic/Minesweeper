f30f5d5b3f9786dccac422ea4734cbcacd391b69
Nicobotic <66079518+Nicobotic@users.noreply.github.com>
2020-05-28 13:28:46 -0400

    Add files via upload

---
+++ src/minsweep/Driver.java
@@ -0,0 +1,8 @@
+package minsweep;
+//main method
+public class Driver {
+	   /** * @param args */   
+	   public static void main(String[] args) {                
+	       InitScreen intzeScrn=new InitScreen();
+	   }
+}
